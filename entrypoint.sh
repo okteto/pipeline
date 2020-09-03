8bad197493399e7fe6308c8713ab51861fbb58cf
Ramiro Berrelleza <rberrelleza@gmail.com>
2020-09-02 17:12:51 -0700

    clean branch name
    Signed-off-by: Ramiro Berrelleza <rberrelleza@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,24 @@ set -ex
 name=$1
 namespace=$2
 repository=$GITHUB_REPOSITORY
-branch=$GITHUB_REF
+
+if [ -z $GITHUB_REF ]; then
+echo "fail to detect branch name"
+exit 1
+fi
+
+branch=$(echo ${GITHUB_REF##*/})
+
+if [ -z $name ]; then
+name="${GITHUB_WORKFLOW}-${GITHUB_RUN_NUMBER}"
+fi
 
 params=""
-
 if [ ! -z $namespace ]; then
 params="--namespac=$namespace"
 fi
 
+
+
 echo running: okteto pipeline on $(pwd)
-okteto create pipeline --branch=${branch} --repository=https://github.com/${repository} ${params} --wait
+okteto create pipeline --name ${name} --branch=${branch} --repository=${GITHUB_SERVER_URL}/${repository} ${params} --wait
