2bad42711f8f5a0f44f09af1a32b6065edfbd9f0
Ramiro Berrelleza <ramiro@okteto.com>
2021-01-22 15:58:04 -0800

    Add timeout to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@ set -ex
 
 name=$1
 namespace=$2
+timeout=$3
 
 if [ -z $GITHUB_REF ]; then
 echo "fail to detect branch name"
@@ -21,6 +22,11 @@ params=""
 if [ ! -z $namespace ]; then
 params="--namespace=$namespace"
 fi
+
+if [ ! -z $timeout ]; then
+params="--timeout=$timeout"
+fi
+
 
 echo running: okteto pipeline deploy --name "${name}" --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" ${params} --wait
 okteto pipeline deploy --name "${name}" --branch="${branch}" --repository="${GITHUB_SERVER_URL}/${repository}" ${params} --wait
