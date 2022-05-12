60c993cbf386b21fd8c855671f59d6d04e66b5aa
Teresa Romero <teresa@okteto.com>
2022-05-12 14:49:49 +0200

    refactor: added crt extension
    Signed-off-by: Teresa Romero <teresa@okteto.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 if [ ! -z "$OKTETO_CA_CERT" ]; then
    echo "Custom certificate is provided"
-   echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert
+   echo "$OKTETO_CA_CERT" > /usr/local/share/ca-certificates/okteto_ca_cert.crt
    update-ca-certificates
 fi
 
